dac5bc4fb8f43293e3d5428d84fa56eb117bb864
Yuyang Zhou <zhouyuyang712@outlook.com>
2022-09-13 10:25:54 +0000

    Add inference script

---
+++ cmd_pretrained_openfold.sh
@@ -0,0 +1,16 @@
+python3 run_pretrained_openfold.py \
+        /root/openfold/fasta_dir\
+        /home/public/OpenFold/dataset/pdb_mmcif/mmcif_files/\
+        --uniref90_database_path  /home/public/OpenFold/dataset/uniref90/uniref90.fasta \
+        --mgnify_database_path  /home/public/OpenFold/dataset/mgnify/mgy_clusters.fa \
+        --pdb70_database_path  /home/public/OpenFold/dataset/pdb70/pdb70 \
+        --uniclust30_database_path  /home/public/OpenFold/dataset/uniclust30/uniclust30_2018_08/uniclust30_2018_08 \
+        --output_dir /root/openfold/output \
+        --bfd_database_path  /home/public/OpenFold/dataset/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
+        --model_device 'cuda:0' \
+        --jackhmmer_binary_path /opt/anaconda3/envs/openfold_venv/bin/jackhmmer \
+        --hhblits_binary_path /opt/anaconda3/envs/openfold_venv/bin/hhblits \
+        --hhsearch_binary_path /opt/anaconda3/envs/openfold_venv/bin/hhsearch \
+        --kalign_binary_path /opt/anaconda3/envs/openfold_venv/bin/kalign \
+        --config_preset 'model_1_ptm' \
+        --openfold_checkpoint_path /home/public/OpenFold/resources/openfold_params/finetuning_ptm_2.pt \

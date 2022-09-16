d024dafaf1d4f5daabe5a13cb6e7f89d37270af6
Yuyang Zhou <zhouyuyang712@outlook.com>
2022-09-16 13:28:19 +0000

    Update running tool

--- cmd_pretrained_openfold.sh
+++ cmd_pretrained_openfold.sh
@@ -1,6 +1,6 @@
 python3 run_pretrained_openfold.py \
-        /root/openfold/fasta_dir\
-        /home/public/OpenFold/dataset/pdb_mmcif/mmcif_files/\
+        /home/public/OpenFold/fasta_dir \
+        /home/public/OpenFold/dataset/pdb_mmcif/mmcif_files/ \
         --uniref90_database_path  /home/public/OpenFold/dataset/uniref90/uniref90.fasta \
         --mgnify_database_path  /home/public/OpenFold/dataset/mgnify/mgy_clusters.fa \
         --pdb70_database_path  /home/public/OpenFold/dataset/pdb70/pdb70 \
@@ -8,9 +8,9 @@ python3 run_pretrained_openfold.py \
         --output_dir /root/openfold/output \
         --bfd_database_path  /home/public/OpenFold/dataset/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
         --model_device 'cuda:0' \
-        --jackhmmer_binary_path /opt/anaconda3/envs/openfold_venv/bin/jackhmmer \
-        --hhblits_binary_path /opt/anaconda3/envs/openfold_venv/bin/hhblits \
-        --hhsearch_binary_path /opt/anaconda3/envs/openfold_venv/bin/hhsearch \
-        --kalign_binary_path /opt/anaconda3/envs/openfold_venv/bin/kalign \
+        --jackhmmer_binary_path /opt/anaconda3/envs/openfold_env/bin/jackhmmer \
+        --hhblits_binary_path /opt/anaconda3/envs/openfold_env/bin/hhblits \
+        --hhsearch_binary_path /opt/anaconda3/envs/openfold_env/bin/hhsearch \
+        --kalign_binary_path /opt/anaconda3/envs/openfold_env/bin/kalign \
         --config_preset 'model_1_ptm' \
-        --openfold_checkpoint_path /home/public/OpenFold/resources/openfold_params/finetuning_ptm_2.pt \
+        --openfold_checkpoint_path /home/public/OpenFold/resources/openfold_params/finetuning_ptm_2.pt
